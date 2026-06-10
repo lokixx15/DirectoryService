ada1254c31fc58e66a3feaeb6cdc0a631b2cbbd5
Vladimir <vladimirvasilev27617@gmail.com>
2026-06-10 19:22:21 +0700

    refactor: remove try-catch from departments/api

--- frontend/src/entities/departments/api.ts
+++ frontend/src/entities/departments/api.ts
@@ -2,27 +2,17 @@ import { apiClient } from "@/shared/api/axios-instance";
 import { DepartmentSummary, GetDepartmentsSummaryRequest } from "./types";
 import { Envelope } from "@/shared/api/envelope";
 import { PaginationResponse } from "@/shared/api/pagination-response";
-import { isAxiosError } from "axios";
 import { keepPreviousData, queryOptions } from "@tanstack/react-query";
 
 export const departmentsApi = {
   getDepartmentsSummary: async (request: GetDepartmentsSummaryRequest) => {
-    try {
-      const response = await apiClient.get<
-        Envelope<PaginationResponse<DepartmentSummary>>
-      >("/departments/summary", {
-        params: request,
-      });
+    const response = await apiClient.get<
+      Envelope<PaginationResponse<DepartmentSummary>>
+    >("/departments/summary", {
+      params: request,
+    });
 
-      return response.data;
-    } catch (error) {
-      if (isAxiosError(error) && error.response?.data) {
-        return error.response.data as Envelope<
-          PaginationResponse<DepartmentSummary>
-        >;
-      }
-      throw error;
-    }
+    return response.data;
   },
 };
 
