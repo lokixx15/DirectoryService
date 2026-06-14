56d8b9ba72560f18413a9ae3c3f7c3d51829b104
Vladimir <vladimirvasilev27617@gmail.com>
2026-06-14 22:29:12 +0700

    Revert "fix: use camelCase JSON serialization in SuccessResult"
    This reverts commit 9a4cd5bc9d0050ffec2463a2d4975273f5c45ce2.

--- backend/SharedService/SharedService.Framework/Endpoints/SuccessResult.cs
+++ backend/SharedService/SharedService.Framework/Endpoints/SuccessResult.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using SharedService.SharedKernel;
 
@@ -7,8 +6,6 @@ namespace SharedService.Framework.Endpoints;
 
 public sealed class SuccessResult : IResult
 {
-    private static readonly JsonSerializerOptions JsonOptions = new(JsonSerializerDefaults.Web);
-
     public async Task ExecuteAsync(HttpContext httpContext)
     {
         ArgumentNullException.ThrowIfNull(httpContext);
@@ -17,13 +14,12 @@ public sealed class SuccessResult : IResult
 
         httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
 
-        await httpContext.Response.WriteAsJsonAsync(envelope, JsonOptions);
+        await httpContext.Response.WriteAsJsonAsync(envelope);
     }
 }
 
 public class SuccessResult<TValue> : IResult
 {
-    private static readonly JsonSerializerOptions JsonOptions = new(JsonSerializerDefaults.Web);
     private readonly TValue _value;
 
     public SuccessResult(TValue value)
@@ -39,6 +35,6 @@ public class SuccessResult<TValue> : IResult
 
         httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
 
-        await httpContext.Response.WriteAsJsonAsync(envelope, JsonOptions);
+        await httpContext.Response.WriteAsJsonAsync(envelope);
     }
 }
